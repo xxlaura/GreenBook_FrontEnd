cb25459409e42f442fbfab204b3e9376731f3f29
Laura Zeng <laurazenginchina@gmail.com>
2021-12-19 16:29:54 +0800

    v1 done

--- app.js
+++ app.js
@@ -25,8 +25,7 @@ App({
   },
 
   globalData: {
-    // baseUrl: 'http://localhost:3000/api/v1',
+    // baseUrl: 'http://localhost:3000/api/v1'
     baseUrl: 'https://green-book-happyhour.herokuapp.com/api/v1'
-    
   }
 })
